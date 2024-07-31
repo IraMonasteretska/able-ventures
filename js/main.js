1ee6422986c09de915b16ecd643050c1848dbb54
IraMonasteretska <44293548+IraMonasteretska@users.noreply.github.com>
2024-07-31 15:53:00 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,18 +1,28 @@
 $(document).ready(function(){
+    // header menu
     $('.dropdown > a').click(function(e){
         e.preventDefault();
         $('.submenu').not($(this).next()).removeClass('active');
         $(this).next('.submenu').toggleClass('active');
 
+        $('.dropdown > a').not($(this)).removeClass('active');
         $(this).toggleClass('active');
     });
 
+    $(document).click(function (event) {
+		let $target = $(event.target);
+		if (!$target.closest('.dropdown > a').length) {
+            $('.submenu').removeClass('active');
+            $('.dropdown > a').removeClass('active');
+		}
+	});
+
+    // header search
     $('header .btnwrap').click(function(){
         $('.searchopen').toggle();
         $('.btnwrap button').toggle();
         $('.header__search input').toggle();
-    })
-
+    });
 
 	$(document).click(function (event) {
 		let $target = $(event.target);
@@ -23,6 +33,7 @@ $(document).ready(function(){
 		}
 	});
 
+    // header 
     $(window).on('scroll load', function () {
         if ($(this).scrollTop() > 10) {
             $('.header').addClass('scroll');
@@ -31,10 +42,13 @@ $(document).ready(function(){
         }
     });
 
+    // burger menu
     $('.burger').click(function(){
         $(this).toggleClass('on');
         $('.header__wrapper').toggleClass('show');
         $('.menubg').toggleClass('active');
+
+        $('.header__navcol').toggleClass('show');
     })
 
 
